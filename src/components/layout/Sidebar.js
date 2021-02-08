c1a93863690bf14b6f595dac44612a8a8e370b8c
molinar1999 <matthew.a.molinar@gmail.com>
2021-02-07 19:56:47 -0600

    added styling and logo

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -4,34 +4,37 @@ import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar } from 'react-i
 export const Sidebar = () => (
 <div className="sidebar" data-testid="sidebar">
     <ul className="sidebar__generic">
-        <li>
+        <li
+        data-testid="inbox"
+        className="inbox"
+        >
             <span>
                 <FaInbox />
             </span>
             <span>Inbox</span>
         </li>
             
-        <li>
+        <li data-testid="today" className="today">
             <span>
                 <FaRegCalendar />
             </span>
             <span>Today</span>
         </li>
-        <li>
+        <li data-testid="next_7" className="next_7">
             <span>
                 <FaRegCalendarAlt />
             </span>
             <span>Next 7 days</span>
         </li>
     </ul>
-    <div className="sidebar__midle">
+    <div className="sidebar__middle">
         <span><FaChevronDown /></span>
         <h2>Projects</h2>
     </div>
 
     <ul className="sidebar__projects">Projects will be here!</ul>
 
-    Add Project Component Here!!
+    TODO: Add Project Component Here!!
 </div>
 );
 
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,16 +1,18 @@
 import { useState, useEffect } from 'react';
+import moment from 'moment';
 import { firebase } from '../firebase';
 import { collatedTasksExist } from '../helpers';
 
 
 export const useTasks = selectedProject => {
     const [tasks, setTasks] = useState([]);
+    const [archivedTasks, setArchivedTasks] = useState([]);
     // do some work in here . . .
     useEffect(() => {
         let unsubscribe = firebase
         .firestore()
-        .collections('tasks')
-        .where('userId', '==', '6273');
+        .collection('tasks')
+        .where('userId', '==', '111');
 
         unsubscribe = selectedProject && !collatedTasksExist(selectedProject) ? 
         (unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
@@ -23,5 +25,62 @@ export const useTasks = selectedProject => {
         : selectedProject === 'INBOX' || selectedProject === 0
         ? (unsubscribe = unsubscribe.where('date', '==', ''))
         : unsubscribe;
+
+        unsubscribe = unsubscribe.onSnapshot(snapshot => {
+            const newTasks = snapshot.docs.map(task => ({
+                id: task.id,
+                ...task.data(),
+            }));
+
+
+            setTasks(
+                selectedProject === 'NEXT_7'
+                ? newTasks.filter(
+                    // filter out the tasks that are less than or equal 7 days away
+                    task => moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 &&
+                    task.archived !== true
+                )
+                : newTasks.filter(task => task.archived !== true)
+            );
+            setArchivedTasks(newTasks.filter(task => task.archived !== false));
+        });
+
+        return () => unsubscribe();
     }, [selectedProject])
+    // Basically, when selectedProject changes, re run all of useTasks.
+
+    return { tasks, archivedTasks };
+
+};
+// const selectedProject = 1;
+// const { tasks, archivedTasks } = useTasks(selectedProject);
+
+
+// we wont be checking for projects live as we are with tasks.
+export const useProjects = () => {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        firebase
+        .firestore()
+        .collection('projects')
+        .where('userId', '==', '111')
+        .orderBy('projectId')
+        .get()
+        .then(snapshot => {
+            const allProjects = snapshot.docs.map(project => ({
+                ...project.data(),
+                docId: project.id,
+            }));
+
+            if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
+                setProjects(allProjects);
+                // This condition protects against an infinite loop. 
+                // Bc useProjects will change everytime projects changes, but
+                // the hook itself changes projects.
+            }
+        })
+    }, [projects]);
+
+    return { projects, setProjects };
 }
